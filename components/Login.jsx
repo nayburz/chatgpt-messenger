0538d51758bd4820c211fe9884dc9a628e66fa72
nayburz <>
2023-10-28 06:39:24 -0700

    Project setup

---
+++ components/Login.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+const Login = () => {
+  return (
+    <div className="bg-[#11A37F] h-screen flex flex-col items-center">
+      <Image src="" width={300} height={300} alt="logo" />
+      <button className="text-white">Login</button>
+    </div>
+  );
+};
+
+export default Login;
